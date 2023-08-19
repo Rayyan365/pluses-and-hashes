0befc40705b4b668ddbc978c8cf914dab1d77fc8
Rayyan365 <rayanabdallah09@gamil.com>
2023-08-19 01:34:49 -0700

    pluses-and-hashes assignment done

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 // Good Luck 💪🏾
+function countChrOccurence ('######+++++++###+') {
+    let charMap = new Map();
+    const count = 0;
+     for (const key of str) {
+      charMap.set(key,count); 
+     }
+   
+     for (const key of str) {
+       let count = charMap.get(key);
+       charMap.set(key, count + 1);
+     }
+   
+   
+     for (const [key,value] of charMap) {
+       console.log(key,value);
+     }
+   
+   }  
